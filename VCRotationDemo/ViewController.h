78d8e909dc9784d48c188fd3a5b1dcbabb5cfe7c
VicChan <victorchanchina@gmail.com>
2016-09-24 13:18:58 +0800

    Initial Commit

---
+++ VCRotationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VCRotationDemo
+//
+//  Created by VicChan on 2016/9/24.
+//  Copyright © 2016年 VicChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VCRotationDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VCRotationDemo
+//
+//  Created by VicChan on 2016/9/24.
+//  Copyright © 2016年 VicChan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
