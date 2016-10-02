be7497ae4072828e23f6983162823602436e92d8
VicChan <victorchanchina@gmail.com>
2016-10-02 08:27:04 +0800

    init

---
+++ VCRotation/VCRotation.h
@@ -0,0 +1,13 @@
+//
+//  VCRotation.h
+//  VCRotationDemo
+//
+//  Created by VicChan on 2016/9/24.
+//  Copyright © 2016年 VicChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCRotation : UIControl
+
+@end
---
+++ VCRotation/VCRotation.m
@@ -0,0 +1,21 @@
+//
+//  VCRotation.m
+//  VCRotationDemo
+//
+//  Created by VicChan on 2016/9/24.
+//  Copyright © 2016年 VicChan. All rights reserved.
+//
+
+#import "VCRotation.h"
+
+@implementation VCRotation
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
